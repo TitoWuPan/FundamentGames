daf3cc53dfc3a6df36d72b57d9cc9282cbff950f
TitoWuPan <91169600+TitoWuPan@users.noreply.github.com>
2023-06-28 12:24:40 -0500

    Add files via upload

--- Motor/MainGame.h
+++ Motor/MainGame.h
@@ -2,6 +2,17 @@
 #include <SDL/SDL.h>
 #include <GL/eglew.h>
 #include "Sprite.h"
+#include "HLSLProgram.h"
+#include "Window.h"
+#include "Camera2D.h"
+#include "InputManager.h"
+#include "Level.h"
+#include "Player.h"
+#include "Human.h"
+#include "Bullet.h"
+#include "Zombie.h"
+#include "SpriteBatch.h"
+#include "SpriteFont.h"
 
 enum class GameState {
 	PLAY,EXIT
@@ -12,17 +23,35 @@ class MainGame
 private:
 	int width;
 	int height;
-	SDL_Window* window;
-	void init();
+	SpriteFont* spriteFont;
+	SpriteBatch spriteBatch;
+	SpriteBatch hudBatch;
+	vector<Level*> levels;
+	vector<Bullet*> bullets;
+	vector<Human*> humans;
+	vector<Zombie*> zombies;
+	Player* player;
+	int currentLevel;
+	Window window;
+	HLSLProgram program;
 	Sprite sprite;
+	Camera2D camera2D;
+	InputManager inputManager;
+	void init();
+	void initLevel();
 	void processInput();
-
+	void initShaders();
+	void handleInput();
+	void updateElements();
+	void createBullet();
 public:
 	MainGame();
 	~MainGame();
 	GameState gameState;
+	
 	void run();
 	void draw();
+	void drawHud();
 	void update();
 };
 
--- Motor/Vertex.h
+++ Motor/Vertex.h
@@ -10,8 +10,11 @@ struct Position {
 	}
 };
 
-
 struct Color {
+	Color(GLubyte _r, GLubyte _g, GLubyte _b, GLubyte _a) {
+		set(_r, _g, _b, _a);
+	}
+	Color() : r(0), g(0), b(0), a(0) {}
 	GLubyte r;
 	GLubyte g;
 	GLubyte b;
@@ -24,14 +27,26 @@ struct Color {
 	}
 };
 
+struct UV {
+	float u;
+	float v;
+	void set(float _u, float _v) {
+		u = _u;
+		v = _v;
+	}
+};
 
 struct Vertex {
 	Position position;
 	Color color;
+	UV uv;
 	void setPosition(float x,float y) {
 		position.set(x, y);
 	}
 	void setColor(GLubyte _r, GLubyte _g, GLubyte _b, GLubyte _a) {
 		color.set(_r, _g, _b, _a);
+	}
+	void setUV(float u, float v) {
+		uv.set(u, v);
 	}
 };
